b7f6d49e8cdb73f727dfe890710a016a42ad7b62
tomOneEightTwo <tomhornbuckle1@gmail.com>
2016-11-09 18:58:24 +0000

    more commitz

--- script.js
+++ script.js
@@ -2,6 +2,5 @@
 
 var today = new Date();
 var year = today.getFullYear();
-
 var foot = document.getElementById('footer');
-foot.innerHTML = '<p>Cleaners R us &copy; ' + year + '</p>';
+foot.innerHTML = '<p>Cleaners R us &copy; ' + year + ' | Handmade By Tom</p>';
