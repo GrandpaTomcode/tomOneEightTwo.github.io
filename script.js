5a801e4c7fd8bdc0fccc5b1cbedf3f41f06dc983
tomOneEightTwo <tomhornbuckle1@gmail.com>
2016-11-15 17:33:04 +0000

    Added Jquery Slideshow

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ var today = new Date();
 var year = today.getFullYear();
 var foot = document.getElementById('footer');
 foot.innerHTML = '<p>Cleaners R us Est: 2015 &copy; ' + year + ' | Handmade By Tom</p>';
+
+// Jquery Fade Slide Show.
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
