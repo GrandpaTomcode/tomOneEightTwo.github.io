96eba936b3737918bbe334dc83af3cd19c0111fd
tomOneEightTwo <tomhornbuckle1@gmail.com>
2016-11-25 17:06:20 +0000

    Completely changed to ui

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 // Footer Copyright year.
+//
+// var today = new Date();
+// var year = today.getFullYear();
+// var foot = document.getElementById('footer');
+// foot.innerHTML = '<p>Cleaners R us Est: 2014 &copy; ' + year + ' | Handmade By Tom</p>';
 
-var today = new Date();
-var year = today.getFullYear();
-var foot = document.getElementById('footer');
-foot.innerHTML = '<p>Cleaners R us Est: 2015 &copy; ' + year + ' | Handmade By Tom</p>';
-
-// Jquery Fade Slide Show.
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-},  5000);
+var bg = jQuery("#colOne");
+jQuery(window).resize("resizeBackground");
+function resizeBackground() {
+    bg.height(jQuery(window).height());
+}
+resizeBackground();
