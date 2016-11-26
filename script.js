f8ce76a315b2409d2f5236bd0e772d9a853bafc0
tomOneEightTwo <tomhornbuckle1@gmail.com>
2016-11-26 20:22:26 +0000

    added sections to page

--- script.js
+++ script.js
@@ -5,9 +5,6 @@
 // var foot = document.getElementById('footer');
 // foot.innerHTML = '<p>Cleaners R us Est: 2014 &copy; ' + year + ' | Handmade By Tom</p>';
 
-var bg = jQuery("#colOne");
-jQuery(window).resize("resizeBackground");
-function resizeBackground() {
-    bg.height(jQuery(window).height());
-}
-resizeBackground();
+$(document).ready(function(){
+    $(".iconsFade, .title, .scroll, .subTitle").delay(500).fadeIn(3000)
+});
