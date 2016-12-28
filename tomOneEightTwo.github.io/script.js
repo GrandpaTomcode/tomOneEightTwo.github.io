dc5086cd0a202f4cf55f6087f08e1ccefebb0f45
Tom Hornbuckle <tomhornbuckle1@gmail.com>
2016-12-28 23:34:47 +0000

    We are live
    we are live

---
+++ tomOneEightTwo.github.io/script.js
@@ -0,0 +1,11 @@
+// Footer Copyright year.
+//
+// var today = new Date();
+// var year = today.getFullYear();
+// var foot = document.getElementById('footer');
+// foot.innerHTML = '<p>Cleaners R us Est: 2014 &copy; ' + year + ' | Handmade By Tom</p>';
+
+// fade in on load
+$(document).ready(function(){
+    $(".iconsFade, .title, .scroll, .subTitle").fadeIn(1000)
+});
